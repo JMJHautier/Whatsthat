88ffdc5f0c11c6cf71527a7bfc7366a46b182c8a
J Hautier <jhautier@hotmail.fr>
2021-04-25 18:21:29 +0200

    style: add Material-UI theme

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Guess from './routes/guess/Guess.js';
 import Search from './routes/search/Search.js';
 import About from './routes/about/About.js';
 import User from './routes/user/User.js';
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="App">
-    
+      <Button primary="light"> Button </Button>
       <Switch>
         <Route exact path="/">
           <Home />
