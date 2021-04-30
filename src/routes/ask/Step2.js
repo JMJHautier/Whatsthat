3e7a3658f7a74b757b267abf0c9f52ae0dd7afab
J Hautier <jhautier@hotmail.fr>
2021-04-30 17:49:11 +0200

    refactor: improve indentation and remove comments from Step2.js

--- src/routes/ask/Step2.js
+++ src/routes/ask/Step2.js
@@ -3,12 +3,12 @@ import Prism from "prismjs";
 import './codeEditor.css';
 import './prism.css'
 import { useForm } from "react-hook-form";
-import ReactQuill, {Quill} from 'react-quill'; 
+// import ReactQuill from 'react-quill'; 
 import 'react-quill/dist/quill.snow.css';
 
 const Step2 = ({language, content, setContent, onSubmit, prevFormStep, whatsthat, setWhatsthat, nextFormStep}) => {
 
-   const { register, handleSubmit, watch, formState: { errors, isValid} } = useForm({mode:"all"});
+const { register, handleSubmit, watch, formState: { errors, isValid} } = useForm({mode:"all"});
 //  const breakdown = Quill.getEditor();
 //  console.log(breakdown);
 //? Using quill to select bits of text. I leave it here for reference, but it is no longer useful
@@ -38,29 +38,27 @@ const Step2 = ({language, content, setContent, onSubmit, prevFormStep, whatsthat
 
       <pre className="line-numbers">
         <code
-         onMouseUp={()=>setWhatsthat(window.getSelection().toString())}
-         className={`language-${language}`}>
-            {content}
-         </code>
+        onMouseUp={()=>setWhatsthat(window.getSelection().toString())}
+        className={`language-${language}`}>
+
+        {content}
+        </code>
       </pre>
 
       <pre> {JSON.stringify(watch(), null, 2)}</pre>
-      <p> you have selected: <strong>{whatsthat}</strong></p>
-
-      {/* <p> You have selected:</p> */}
-
+      <h3> you have selected: <em>{whatsthat}</em></h3>
       <input
         hidden="true"
-         disabled="true"
-         {...register("Whatsthat", { required: {value: true, message:"Please pick a word" }})} value={whatsthat}
-         />
-         {errors.Whatsthat && <p>{errors.Whatsthat.message}</p>}
+        disabled="true"
+        {...register("Whatsthat", { required: {value: true, message:"Please pick a word" }})} value={whatsthat}
+        />
+        {errors.Whatsthat && <p>{errors.Whatsthat.message}</p>}
 
       {whatsthat && 
       (<input 
-         type="submit" 
-         onClick={nextFormStep} 
-         value="What's that"/>)}
+        type="submit" 
+        onClick={nextFormStep} 
+        value="What's that"/>)}
     </form>
 
   );
