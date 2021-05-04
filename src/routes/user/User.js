00d372b026f10930494646249b5a9d6d5fd39487
J Hautier <jhautier@hotmail.fr>
2021-05-04 22:16:15 +0200

    feat: create layout user profile

--- src/routes/user/User.js
+++ src/routes/user/User.js
@@ -1,6 +1,18 @@
-const User = () => {
+import {useContext} from 'react';
+import {AuthContext} from '../../context/AuthContext'
 
-   return <div>User page</div> 
+const User = () => {
+const {user} = useContext(AuthContext);
+const {username, email} = user;
+console.log(user);
+const serverLink = process.env.ORIGIN || "http://localhost:3001";
+
+
+   return (<div> <h3>User page</h3>
+ <ul> 
+   <li> username: {username}</li>
+   </ul></div>)
+   
 }
 
 export default User;
