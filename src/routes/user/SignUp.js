df5406fe40a5e55c882fadf2ae9bd4533c51f561
J Hautier <jhautier@hotmail.fr>
2021-05-11 23:17:31 +0200

    fix: reduce again delay after signing up

--- src/routes/user/SignUp.js
+++ src/routes/user/SignUp.js
@@ -51,7 +51,7 @@ if(isAuthenticated) {
    // setReload(false)
    const serverLink = process.env.REACT_APP_FRONT || "http://localhost:3000";
 
-   setTimeout(()=> { window.location.href = serverLink }, 25000)
+   setTimeout(()=> { window.location.href = serverLink }, 2500)
    return <h4> You have successfuly registered! You will be soon redirected </h4> 
 
 }
